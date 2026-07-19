cf262995cedc832141a821f574eedf3e63a2c906
Hemant-Nikam <hrn2501@gmail.com>
2026-07-19 11:18:17 +0530

    Add: boats-to-save-people - submission-0

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0;
+        int j = people.length -1;
+        int cnt = 0;
+
+        while(i <= j){
+            if(people[j] + people[i] <= limit){
+                i++;
+            }
+            j--;
+            cnt++;
+        }
+        return cnt;
+    }
+}
