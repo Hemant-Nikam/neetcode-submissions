c48e64da5bb6e6775aef7d3bc1bbe709b84c9158
Hemant-Nikam <hrn2501@gmail.com>
2026-07-27 14:37:34 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int l = 0;
+        int r = 1;
+        while(r < prices.length){
+            if(prices[l] > prices[r]){
+                l = r;
+                r++;
+            }else{
+                int profit = prices[r] - prices[l];
+                if(profit > maxProfit){
+                    maxProfit = profit;
+                }
+                r++;
+            }
+        }
+        return maxProfit;
+    }
+}
